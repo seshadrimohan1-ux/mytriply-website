8243a2edf3ee24c4d2a4c584236c6d368c9416c9
seshadrimohan1-ux <seshadrimohan1@gmail.com>
2026-07-05 15:48:06 -0400

    Update planner.js

--- planner.js
+++ planner.js
@@ -2,6 +2,6 @@ const button = document.getElementById("tripButton");
 
 button.addEventListener("click", function(){
 
-    alert("Eventually this will generate your trip!");
+    alert("Bloated yet again!");
 
 });
