e64bb038cdfbbf7d899a0e271c8e380b430a89ee
seshadrimohan1-ux <seshadrimohan1@gmail.com>
2026-07-15 12:07:49 -0400

    new map

--- planner.js
+++ planner.js
@@ -1,6 +1,36 @@
+const map = L.map('map').setView([20, 0], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+    maxZoom:19,
+
+    attribution:'© OpenStreetMap contributors'
+
+}).addTo(map);
+
+let marker;
+
+map.on('click', function(e){
+
+    const lat = e.latlng.lat.toFixed(6);
+    const lng = e.latlng.lng.toFixed(6);
+
+    document.getElementById("latitude").value = lat;
+    document.getElementById("longitude").value = lng;
+
+    if(marker){
+
+        map.removeLayer(marker);
+
+    }
+
+    marker = L.marker([lat,lng]).addTo(map);
+
+});
+
 const button = document.getElementById("tripButton");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function(){
 
     window.location.href = "itinerary.html";
 
